f4f5c7f1f210562f6c3df57db62aae9dce24ac98
Louis Beaudoin <louis@embedded-creations.com>
2016-01-06 15:55:03 -0500

    remove DotStar library

--- apa102-gbctest.ino
+++ apa102-gbctest.ino
@@ -4,8 +4,11 @@
 //
 //
 
-#include <Adafruit_DotStar.h>
-#include <SPI.h>         // COMMENT OUT THIS LINE FOR GEMMA OR TRINKET
+// teensy 3.1 pinout
+// DATA 11
+// CLK  13
+
+#include <SPI.h>
 
 
 uint16_t led16bit_r;
@@ -16,22 +19,13 @@ uint16_t led16bit_b;
 
 #include "color_funcs.h"
 
-#define NUMPIXELS 30 // Number of LEDs in strip
-
-// Here's how to control the LEDs from any two pins:
-#define DATAPIN    5
-#define CLOCKPIN   2
-Adafruit_DotStar strip = Adafruit_DotStar(
-    NUMPIXELS, DATAPIN, CLOCKPIN, DOTSTAR_BRG);
-
 rgbuint16_t c1 = {0,0,0};
 rgbuint16_t c2;
 int steps = 1000;
 
 
 void setup() {
-    strip.begin(); // Initialize pins for output
-    strip.show();  // Turn all LEDs off ASAP
+    SPI.begin();
     rgb_setCurr( &c1 );
 }
 
@@ -51,13 +45,13 @@ void loop() {
     rgb_setDest( &c2, steps );  
 
     for( int j=0; j<steps+1; j++ ) {
-        for(i=0; i<4; i++) strip.sw_spi_out(0);    // Start-frame marker
+        for(i=0; i<4; i++) SPI.transfer(0);    // Start-frame marker
 
         // first LED in chain with 8-bit color
-        strip.sw_spi_out(0xFF);                //  Pixel start with max global brightness
-        strip.sw_spi_out(led16bit_r >> 8); // R,G,B
-        strip.sw_spi_out(led16bit_g >> 8);
-        strip.sw_spi_out(led16bit_b >> 8);
+        SPI.transfer(0xFF);                //  Pixel start with max global brightness
+        SPI.transfer(led16bit_r >> 8); // R,G,B
+        SPI.transfer(led16bit_g >> 8);
+        SPI.transfer(led16bit_b >> 8);
 
         uint8_t globalbrightness;
         uint8_t localshift;
@@ -84,12 +78,12 @@ void loop() {
         }
 
         // drive second pixel with 8-bit color plus 5-bit global brightness
-        strip.sw_spi_out(0xE0 | globalbrightness);                //  Pixel start with global brightness
-        strip.sw_spi_out(led16bit_r >> localshift); // R,G,B
-        strip.sw_spi_out(led16bit_g >> localshift);
-        strip.sw_spi_out(led16bit_b >> localshift);
+        SPI.transfer(0xE0 | globalbrightness);                //  Pixel start with global brightness
+        SPI.transfer(led16bit_r >> localshift); // R,G,B
+        SPI.transfer(led16bit_g >> localshift);
+        SPI.transfer(led16bit_b >> localshift);
 
-        for(i=0; i<1; i++) strip.sw_spi_out(0xFF); // End-frame marker 
+        for(i=0; i<1; i++) SPI.transfer(0xFF); // End-frame marker 
 
         rgb_updateCurrent();
 
